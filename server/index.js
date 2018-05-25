64aa40464b6606ff3630e15228974ac38c82fdcf
jedwardnyc <jacobedwardrico@gmail.com>
2018-05-25 19:41:48 -0400

    hr!

--- server/index.js
+++ server/index.js
@@ -8,13 +8,17 @@ app.use(require('body-parser').json());
 
 app.use('/v1', require('./routes'));
 
-app.get('/', (req, res, next) => {
-  res.send('Capstone is fun');
-});
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+app.use('/vendor', express.static(path.join(__dirname, '../node_modules')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 // app.get('/', (req, res, next) => {
-//   res.sendFile(path.join(__dirname, '../src/index.html'));
+//   res.send('Capstone is fun');
 // });
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
