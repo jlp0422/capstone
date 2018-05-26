b0d5ddd5d4b911b9cc09d2dca349393dbee18ff6
ajkatom <alon_adelson@yahoo.com>
2018-05-25 22:51:33 -0400

    sideBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink, Route, HashRouter as Router, Switch } from 'react-router-dom';
+import SideNav from './SideNav';
 
 const App = () => {
-  return <hr />
-}
+  return (
+    <Router>
+      <div>
+        <SideNav />
+        <Switch>
+          <Route path="/" render={() => <hr />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
