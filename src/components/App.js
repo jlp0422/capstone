bb61a117dbe8fe793a0a253897290c7e0df97954
ajkatom <alon_adelson@yahoo.com>
2018-05-26 13:46:19 -0400

    categories

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { NavLink, Route, HashRouter as Router, Switch } from 'react-router-dom';
+import SideNav from './SideNav';
+import Categories from './Categories';
 
 const App = () => {
-  return <hr />
-}
+  return (
+    <Router>
+      <div>
+        <SideNav />
+        <Switch>
+          <Route path="/" exact render={() => <hr />} />
+          <Route path="/categories" exact render={() => <Categories />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
