5539e453b2b96dcc185148ccb48595dbd31c0c06
Edward Timmer <edtimmer@gmail.com>
2018-06-13 20:09:40 -0400

    links in footer open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <div className="container footer">
       <div>
-        <p>&copy; UnTapped Trivia & <a href="https://www.fullstackacademy.com/">Fullstack Academy</a></p>
+        <p>&copy; UnTapped Trivia & <a href="https://www.fullstackacademy.com/" rel="noopener noreferrer" target="_blank">Fullstack Academy</a></p>
         <p>
-          <a href="https://www.linkedin.com/in/alon-j-adelson-5691b87/">Alon J. Adelson</a>
+          <a href="https://www.linkedin.com/in/alon-j-adelson-5691b87/" rel="noopener noreferrer" target="_blank">Alon J. Adelson</a>
           <span> | </span>
-          <a href="https://www.linkedin.com/in/jeremyphilipson/">Jeremy Philipson</a>
+          <a href="https://www.linkedin.com/in/jeremyphilipson/" rel="noopener noreferrer" target="_blank">Jeremy Philipson</a>
           <span> | </span>
-          <a href="https://www.linkedin.com/in/jacobrico/">Jacob Rico</a>
+          <a href="https://www.linkedin.com/in/jacobrico/" rel="noopener noreferrer" target="_blank">Jacob Rico</a>
           <span> | </span>
-          <a href="https://www.linkedin.com/in/edward-timmer/">Edward Timmer</a>
+          <a href="https://www.linkedin.com/in/edwardtimmer/" rel="noopener noreferrer" target="_blank">Edward Timmer</a>
         </p>
       </div>
     </div>
